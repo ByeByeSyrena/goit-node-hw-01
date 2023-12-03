d099838a5e66a657f5af603851b9f4bdeb10ad00
Ira S <iryna.shevchenko2@gmail.com>
2023-12-03 18:07:29 +0200

    2 step in pr

--- db/contacts.js
+++ db/contacts.js
@@ -2,23 +2,30 @@ const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.join(__dirname, "contacts.json");
 
-async function listContacts() {
+const listContacts = async () => {
   try {
     const readResult = await fs.readFile(contactsPath, "utf-8");
+    console.log(readResult);
     const result = JSON.parse(readResult);
-    console.table(result);
+    // console.table(result);
     return result;
   } catch (err) {
     console.log(err);
     return [];
   }
-}
+};
 
 async function getContactById(contactId) {
   try {
     const contacts = await listContacts();
-    const contact = contacts.find(({ id }) => id === contactId);
-    console.table(contact);
+    const contact = contacts.find(({ contact }) => contact.id === contactId);
+
+    if (contact) {
+      console.log(contact);
+    } else {
+      console.log(`Contact with ID ${contactId} not found.`);
+    }
+
     return contact;
   } catch (err) {
     console.log(err);
@@ -28,8 +35,16 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
   try {
-    // let contacts = await listContacts();
-    // contacts = contacts.filter(({ id }) => id !== contactId);
+    let contacts = await listContacts();
+    const contact = contacts.filter(({ id }) => id !== contactId);
+    console.log(contact);
+    fs.writeFile(contactsPath, newContacts, "utf8", (err) => {
+      if (err) {
+        console.error("Error writing to file:", err);
+      } else {
+        console.log("File written successfully.");
+      }
+    });
   } catch (err) {
     console.log(err);
     return [];
